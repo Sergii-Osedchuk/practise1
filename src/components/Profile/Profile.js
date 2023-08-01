6968129d5522eec4686302bd97aa87de4dc39267
Sergio <norgestream@gmail.com>
2023-08-01 15:09:42 +0300

    first commit

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,33 @@
+function Profile ({username, tag, location, avatar, stats}) {
+    return (
+        <div className='profile'>
+            <div className='description'>
+                <img 
+                  src={avatar} 
+                  alt='User avatar'
+                  className='avatar'
+                />
+                <p className='name'>{username}</p>
+                <p className='tag'>{tag}</p>
+                <p className='location'>{location}</p>
+            </div>
+            <ul className='stats'>
+                <li>
+                    <span class='label'>Followers</span>
+                    <span class='quantity'>{stats.followers}</span>
+                </li>
+                <li>
+                    <span class='label'>Views</span>
+                    <span class='quantity'>{stats.views}</span>
+                </li>
+                <li>
+                    <span class='label'>Likes</span>
+                    <span class='quantity'>{stats.likes}</span>
+                </li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Profile;
