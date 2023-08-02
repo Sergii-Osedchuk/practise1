6cb1a2b32995400aab51551fb6acae3582d757bf
Sergio <norgestream@gmail.com>
2023-08-02 19:42:22 +0300

    stats added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Profile from './components/Profile/Profile'
+import Statistics from './components/Statistics/Statistics'
 import user from './user.json'
+import data from './data.json'
 
 function App() {
   return (
-    <Profile 
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile 
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics 
+        title='UPLOAD STATS'
+        stats={data}
+      />
+      <Statistics 
+        stats={data}
+      />
+    </>
   );
 }
 
