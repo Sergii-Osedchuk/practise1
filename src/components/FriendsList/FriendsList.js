c70d88b9ad82ccbe2e0c50bc3dd941401a8dd08d
Sergio <norgestream@gmail.com>
2023-08-03 22:21:33 +0300

    friendsList

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -3,17 +3,19 @@ import s from './FriendsList.module.css'
 
 function FriendsList({friends}) {
     return (
-        <ul className={s.friendsList}>
-            {friends.map(friend => 
-            <li key={friend.id}>
-                <FriendsListItem 
-                  avatar={friend.avatar} 
-                  name={friend.name}
-                  isOnline={friend.isOnline}
-                />
-            </li>
-            )}
-        </ul>
+        <div className={s.container}>
+          <ul className={s.friendsList}>
+              {friends.map(friend => 
+              <li key={friend.id}>
+                  <FriendsListItem 
+                    avatar={friend.avatar} 
+                    name={friend.name}
+                    isOnline={friend.isOnline}
+                  />
+              </li>
+              )}
+          </ul>
+        </div>
     )
 }
 
